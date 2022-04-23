a80c5de7dc5ea02a21d00ecf49c7e484a433845e
Martin Weinzierl (Gumbini) <35155124+Gumbini@users.noreply.github.com>
2022-04-24 00:54:04 +0200

    Random source, Empty file, More colored output
    - Add more colored output
    - Add empty file error handling
    - Add option for /dev/urandom source
    - Remove O_CLOEXEC flag

--- nina.c
+++ nina.c
@@ -9,16 +9,27 @@
 
 #define A_RST "\033[0m"
 
+#define A_CYN "\033[1;36m"
 #define A_GRN "\033[1;32m"
 #define A_RED "\033[1;31m"
 #define A_WHT "\033[1;97m"
 #define A_YLW "\033[1;33m"
 
+static int inFd  = -1;
+static int outFd = -1;
+
 static void die(const char *msg) {
 	if (errno) {
-		fprintf(stderr, A_RED "[FAIL!]" A_WHT " (%s) %s" A_RST "\n", msg, strerror(errno));
+		fprintf(stderr, A_RED "[FAIL]" A_WHT " (%s) %s" A_RST "\n", msg, strerror(errno));
 	} else {
-		fprintf(stderr, A_RED "[FAIL!]" A_WHT " %s" A_RST "\n", msg);
+		fprintf(stderr, A_RED "[FAIL]" A_WHT " %s" A_RST "\n", msg);
+	}
+
+	if (inFd != -1) {
+		close(inFd);
+	}
+	if (outFd != -1) {
+		close(outFd);
 	}
 
 	exit(EXIT_FAILURE);
@@ -31,7 +42,7 @@ int main(int argc, char **argv) {
 	 A_YLW "  _ __  _ _ __   ____   n" A_RST "uke           |  and destruction\n"
 	 A_YLW " | '_ \\| | '_ \\ / _  |  a" A_RST "pplication    |  software\n"
 	 A_YLW " | | | | | | | | (_| |  " A_RST "\n"
-	 A_YLW " |_| |_|_|_| |_|\\____|  " A_GRN "(c) 2022 Gumbini""\n\n");
+	 A_YLW " |_| |_|_|_| |_|\\____|  " A_CYN "(c) 2022 Gumbini" A_RST "\n\n");
 
 	if (argc <= 0) {
 		errno = EINVAL;
@@ -42,44 +53,70 @@ int main(int argc, char **argv) {
 		die("Missing argument");
 	}
 
-	int fd = open(argv[1], O_WRONLY | O_CLOEXEC);
-	if (fd == -1) {
+	outFd = open(argv[1], O_WRONLY);
+	if (outFd == -1) {
 		die("open");
 	}
-	fprintf(stderr, "File: \"%s\"\n", argv[1]);
 
 	struct stat statbuf;
-	if (fstat(fd, &statbuf) == -1) {
+	if (fstat(outFd, &statbuf) == -1) {
 		die("fstat");
 	}
-	fprintf(stderr, "Size: %li Bytes. Proceed to nuke the file to nirvana? [yes/*]\n", statbuf.st_size);
+	char writeBuf[statbuf.st_blksize];
+	char inputBuf[8];
 
-	char input[8];
-	if (fgets(input, 8, stdin) == NULL) {
+	fprintf(stderr, "File: %s | ", argv[1]);
+	if (statbuf.st_size == 0) {
+		fprintf(stderr, A_RED "Size: 0 Bytes (Empty file!) " A_RST "\n");
+		die("Cannot nuke empty file!");
+	}
+	fprintf(stderr, "Size: %li Byte(s)" A_RST "\n", statbuf.st_size);
+
+	fprintf(stderr, A_WHT "Proceed to nuke the file to nirvana?" A_RST " [yes/*]\n");
+
+	if (fgets(inputBuf, 8, stdin) == NULL) {
 		die("fgets");
 	}
-	input[strcspn(input, "\r\n")] = '\0';
+	inputBuf[strcspn(inputBuf, "\r\n")] = '\0';
 
-	if (strcmp(input, "yes") != 0) {
+	if (strcmp(inputBuf, "yes") != 0) {
 		abort();
 	}
 
-	char buf[statbuf.st_blksize];
-	memset(buf, '\0', statbuf.st_blksize);
+	fprintf(stderr, A_WHT "Choose method" A_RST ": [0] /dev/zero, [1] /dev/urandom\n");
+	if (fgets(inputBuf, 8, stdin) == NULL) {
+		die("fgets");
+	}
+	inputBuf[strcspn(inputBuf, "\r\n")] = '\0';
+	if (strcmp(inputBuf, "0") == 0) {
+		inFd = open("/dev/zero", O_RDONLY);
+	} else if (strcmp(inputBuf, "1") == 0) {
+		inFd = open("/dev/urandom", O_RDONLY);
+	} else {
+		abort();
+	}
+
+	if (inFd == -1) {
+		die("open");
+	}
 
 	for (off_t i = 0; i < statbuf.st_size; i += statbuf.st_blksize) {
-		if (write(fd, buf, statbuf.st_blksize) == -1) {
+		if (read(inFd, writeBuf, statbuf.st_blksize) == -1) {
+			die("read");
+		}
+		if (write(outFd, writeBuf, statbuf.st_blksize) == -1) {
 			die("write");
 		}
 	}
-	fprintf(stderr, "Successfully overwritten.\n");
+	fprintf(stderr, "[" A_GRN "SUCCESS" A_RST "] write: Overwrite data\n");
 
-	if (fsync(fd) == -1) {
+	if (fsync(outFd) == -1) {
 		die("fsync");
 	}
-	fprintf(stderr, "The Kernel has confirmed that all modified data has been written "
-					"to the associated disk device.\n");
+	fprintf(stderr, "[" A_GRN "SUCCESS" A_RST "] fsync: Synchronize state with storage device\n");
 
-	close(fd);
+	close(inFd);
+	close(outFd);
+
 	return EXIT_SUCCESS;
 }
